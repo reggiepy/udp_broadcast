19acccb8c79b8c1e5fa20976d4273483678eb8e7
reggie <wangtongpy@foxmail.com>
2019-09-26 17:43:02 +0800

    update

--- broadcast_client.py
+++ broadcast_client.py
@@ -43,16 +43,15 @@ if __name__ == '__main__':
     parser.add_argument('-a', '--action', help='the action you want to send', default="cmd")
     parser.add_argument('-c', '--cmd', help='the command you want to execute')
     args = parser.parse_args()
-
     print(args.action, args.cmd)
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.settimeout(5)
+    PORT = 1060
 
     ipv4_broadcasts = get_ipv4_broadcasts()
     print(ipv4_broadcasts)
-
-    PORT = 1060
-
     # network = '<broadcast>'
     # network = '192.168.1.255'
     for network in ipv4_broadcasts:
@@ -61,7 +60,6 @@ if __name__ == '__main__':
             'params': args.cmd
         }
         s.sendto(pickle.dumps(sent_data), (network, PORT))
-        s.settimeout(5)
         rec = s.recv(65535)
         b = pickle.loads(rec)
         print("data:", b['data']['result'])
